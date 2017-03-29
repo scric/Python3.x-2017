6cf4e00cb06e170c8f91ef56e894c0e463dca7e1
scric <944242310@qq.com>
2017-03-29 21:35:41 +0800

    no message

--- begginger/2017-03-01/中国大学MOOC-第一周作业/九九乘法表输出.py
+++ begginger/2017-03-01/中国大学MOOC-第一周作业/九九乘法表输出.py
@@ -1,4 +1,18 @@
-for i in range(1,10):
-    for j in range(1,i+1):
-        print('{}*{}={:2} '.format(j,i,i*j),end='') #第三个大括号里的":2" 可以去除掉或者将数字改成其他。应该只是起到位宽的作用。
+for i in range(1, 10):
+    for j in range(1, i+1):
+        print('{}*{}={:2} '.format(j, i, i*j), end='')  # 第三个大括号里的":2" 可以去除掉或者将数字改成其他。应该只是起到位宽的作用。
+    print('')
+
+print()
+
+for i in range(1, 10):
+    for j in range(1, i+1):
+        print('{}*{}={:2} '.format(i, j, i*j), end='')
+    print('')
+
+
+for i in range(1, 10):
+    for j in range(1, i+1):
+        print('{}*{}={:2} '.format(j, i, i*j), end='\n')
+
     print('')
