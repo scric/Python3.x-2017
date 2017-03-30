88b56a74764f0db52c2149aa565394aff5f64e7e
scric <944242310@qq.com>
2017-03-30 22:01:42 +0800

    no message

---
+++ begginger/问题解答/input.py
@@ -0,0 +1,13 @@
+import math
+
+# a = input("请输入一个数字:")
+# print(type(a))
+#
+# print(math.sqrt(a)) # TypeError: must be real number, not str
+
+b = eval(input("请再输入一个数字:"))
+print(type(b))
+
+print(math.sqrt(b))
+
+
